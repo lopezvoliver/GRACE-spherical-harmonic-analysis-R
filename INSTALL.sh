feebec080646c3b28b0ca563e16093f871dd7c55
Oliver Lopez <lopezvoliver@gmail.com>
2016-02-06 18:51:10 +0300

    Modified INSTALL.sh to move FORTRAN binaries to ./bin and perform test

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,6 +4,10 @@ cd ./snake_src
 make
 make -f Makefile_SHA 
 make -f Makefile_VG
-mv snakeSHAS ../
-mv snakeSHA ../
-mv snakeVGRACE ../
+mkdir ../bin
+mv snakeSHAS ../bin/
+mv snakeSHA ../bin/
+mv snakeVGRACE ../bin/
+cd ../bin
+#Perform test
+./snakeSHAS et ../snake_test/grdata.nc ../snake_test/test_flm.nc ../snake_test/test_check.nc 60
